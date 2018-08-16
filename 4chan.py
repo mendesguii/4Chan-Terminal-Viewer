fb5c49622ae8322e5c0b7cc27fcc3a2182abc2f1
Guilherme Mendes <guilhermemendes789@gmail.com>
2018-08-16 17:43:40 +0000

    Function only added

--- 4chan.py
+++ 4chan.py
@@ -57,6 +57,29 @@ def thread_page(query_choice,thread_list,thread_num):
         print('||'+(re.sub("(.{80})", "\\1\n", answer_text.replace(number_t_page,('\x1b[1;31;40m' +'OP'+'\x1b[0m'+' ')), 0, re.DOTALL)))
     #print(list(set(thread_num)))
 
+def only_user_thread(thread,number_user,thread_num):
+    source_thread = requests.get('http://boards.4chan.org/g/thread/'+thread).text
+    soup_thread = BeautifulSoup(source_thread,"html5lib")
+    title_t_page = soup_thread.find('blockquote', class_='postMessage').text
+    number_t_page = soup_thread.find('blockquote', class_='postMessage')['id'].replace('m', '')
+    os.system('clear')
+    print('N# '+number_t_page+' '+'\x1b[1;31;40m' +'OP'+'\x1b[0m'+' '+'\n')
+    print(title_t_page)
+    for each_answer in soup_thread.find_all('div',class_='postContainer replyContainer'):            
+        answer_text = each_answer.find('blockquote',class_='postMessage').text
+        number_answer = each_answer.find('blockquote',class_='postMessage')['id'].replace('m','')
+
+        if number_answer in thread_num:
+            number_answer = thread_num_c[thread_num.index(number_answer)]+number_answer+ '\x1b[0m'
+        for num in thread_num:
+            if num in answer_text:
+                answer_text= answer_text.replace(num,thread_num_c[thread_num.index(num)]+num+ '\x1b[0m'+' ')
+                #break
+        if (number_user in number_answer or number_user in answer_text):
+            print('======================')
+            print('||'+number_answer)  
+            print('||'+(re.sub("(.{80})", "\\1\n", answer_text.replace(number_t_page,('\x1b[1;31;40m' +'OP'+'\x1b[0m'+' ')), 0, re.DOTALL)))
+        
 
 def main():
     source = requests.get('http://boards.4chan.org/g/').text
@@ -74,4 +97,12 @@ def main():
             cmd = input('Command: ')
         if 'back' in cmd:
             main()
+        if 'only' in cmd:
+            cmd = cmd.split(' ')
+            only_user_thread(thread_list[int(query_choice)],str(cmd[1]),thread_num)
+            cmd = input('Command: ')
+            if 'back' in cmd:
+                 thread_page(query_choice,thread_list,thread_num)
+                 cmd = input('Command: ')
+            
 main()
